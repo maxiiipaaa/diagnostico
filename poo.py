c9d2c8c94caffc0cb27e11300441d9e3da8b6b63
maxiiipaaa <137796434+maxiiipaaa@users.noreply.github.com>
2024-03-11 07:26:23 -0700

    Create poo.py

---
+++ poo.py
@@ -0,0 +1,11 @@
+
+
+auto = vehiculo ("ABC123", "toyota", 50000)
+
+print(auto.get_patente())
+
+auto1.set_marca("fiat")
+print(auto1.get_marca())
+
+auto1.set_kilometraje(55000)
+print(auto1.get_kilometraje())
