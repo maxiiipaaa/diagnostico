9ca9c3adbd6d3c3ad312016377bb6a195e7d0058
maxiiipaaa <137796434+maxiiipaaa@users.noreply.github.com>
2024-03-11 07:26:57 -0700

    Create switch.py

---
+++ switch.py
@@ -0,0 +1,22 @@
+numero = int(input("Ingrese un número del 1 al 7: "))
+
+
+elif numero == 1:
+    dia_semana = "lunes"
+elif numero == 2:
+    dia_semana = "martes"
+elif numero == 3:
+    dia_semana = "miércoles"
+elif numero == 4:
+    dia_semana = "jueves"
+elif numero == 5:
+    dia_semana = "viernes"
+elif numero == 6:
+    dia_semana = "sábado"
+elif numero == 7:
+    dia_semana = "domingo"
+else:
+    dia_semana = "número inválido"
+
+
+print("El día de la semana correspondiente es:", dia_semana)
